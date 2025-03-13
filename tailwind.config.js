b93c94f8998acf13067eb149bccab23f1e75ea2c
HamelaSala <salahamela3@gmail.com>
2025-03-13 18:40:33 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./Views/**/*.cshtml", // All Razor View files
+        "./wwwroot/**/*.html"  // Any static HTML in wwwroot
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
